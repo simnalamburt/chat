ed78e49114c63b96283f0f7922fc8374cfd2d2c9
Hyeon Kim <simnalamburt@gmail.com>
2016-04-09 09:23:46 +0900

    index.html에서 webpack 빌드 결과를 쓰도록 설정

--- client/src/main.js
+++ client/src/main.js
@@ -6,8 +6,8 @@ import './main.styl'
 
 ReactDOM.render(
   <div>
-    <h1>TODO!</h1>
-    <p>This will be an amazing TODO webapp</p>
+    <h1>Chatting!</h1>
+    <p>This will be an amazing chatting app</p>
   </div>,
   document.getElementById('example')
 );
