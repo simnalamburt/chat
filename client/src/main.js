9590c7e3f92a72c271aaf59fd14fe4576aa344da
Hyeon Kim <simnalamburt@gmail.com>
2016-04-09 09:21:32 +0900

    Enable webpack

---
+++ client/src/main.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import './main.styl'
+
+ReactDOM.render(
+  <div>
+    <h1>TODO!</h1>
+    <p>This will be an amazing TODO webapp</p>
+  </div>,
+  document.getElementById('example')
+);
---
+++ client/webpack.config.js
@@ -0,0 +1,41 @@
+// @flow
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Always enabled plugins
+const plugs = [
+  new ExtractTextPlugin('_bundle.css')
+];
+
+// Production only plugins
+const prod = [
+  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
+];
+
+module.exports = {
+  context: `${__dirname}/src`,
+  entry: './main.js',
+  output: {
+    path: `${__dirname}/../server/public/build`,
+    publicPath: '/build/',
+    filename: '_bundle.js'
+  },
+  devtool: 'source-map',
+  plugins: process.env.NODE_ENV !== 'production' ? plugs : plugs.concat(prod),
+  module: {
+    loaders: [
+      { test: /\.png$/, loader: 'file?name=static/[hash].[ext]' },
+      { test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file?name=static/[hash].[ext]' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
+      { test: /\.styl$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!stylus') },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: ['es2015', 'stage-3', 'react'] }
+      }
+    ]
+  },
+  postcss: ()/*: Array<Object>*/ => [autoprefixer]
+};
