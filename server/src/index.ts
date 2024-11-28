cf48c6ed0a304984e79f421d7af8578bfc664599
Hyeon Kim <simnalamburt@gmail.com>
2024-11-28 18:33:33 +0900

    server: Finish implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,26 @@
 import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(4567);
+const sockets = new Map;
+
+const app = new Elysia()
+  .use(staticPlugin({ prefix: "/" }))
+  .ws("/api", {
+    open(ws) {
+      sockets.set(ws.id, ws);
+    },
+    close(ws) {
+      sockets.delete(ws.id);
+    },
+    message(ws, message) {
+      for (const [id, socket] of sockets) {
+        if (ws.id === id) continue;
+
+        socket.send(message);
+      }
+    },
+  })
+  .listen(4567);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
